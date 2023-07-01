cc4f0dbf69ce33c6e52bee28ce4343b5a0532b54
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-07-01 18:10:44 +0300

    Hello world

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_articles():
+    return ({"Hello":"World"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
